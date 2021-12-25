c6a92d6b7e70b1381aecd9203c60047c721adb50
dkdrlahWl <63003501+dkdrlahWl@users.noreply.github.com>
2021-12-25 15:29:47 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -43,12 +43,14 @@ async def on_message(message):
         embed = discord.Embed(title="명령어", description="봇 명령어", color=0x62C1CC)
         embed.add_field(name="도박", value="돈, 돈받기, 도박 <금액>, 올인, 랭킹, 레벨랭킹, 송금 <금액> <@대상>, 상점, 예시 : .레벨업팩1 할시 바로 구매 후 사용", inline=True)
         await message.channel.send("", embed=embed)
+        return
 
 
     if cmd == prefix + "상점" or cmd == prefix + "ㅅㅈ":
         embed = discord.Embed(title="상점", description="", color=0x62C1CC)
         embed.add_field(name="list", value="레벨업팩 1급 : 5만 | 1 ~ 2업 \n\n 레벨업팩 2급 : 20만 | 1 ~ 10업\n\n 레벨업팩 3급 : 100만 | 1 ~ 50업\n\n 레벨업팩 4급 : 300만 | 1 ~ 150업\n\n 레벨업팩 5급 : 900만 | 1 ~ 450업\n\n 레벨업팩 6급 : 5000만 | 1 ~ 5000업\n\n 레벨업팩 7급 : 5억 | 1 ~ 50000업\n\n 레벨업팩 8급 : 55억 | 1 ~ 450000업\n", inline=True)
         await message.channel.send("", embed=embed)
+        return
 
 
     if cmd == prefix + "돈받기" or cmd == prefix + "ㄷㅂㄱ":
@@ -90,7 +92,7 @@ async def on_message(message):
         elif not ID in idA:  # ID가 없을 때
             embed = discord.Embed(title="", description="0 원", color=0xFF00FF)
             await message.channel.send(embed=embed)
-
+        return
 
 
     if cmd == prefix + "올인" or cmd == prefix + "ㅇㅇ":
@@ -145,6 +147,8 @@ async def on_message(message):
             await count.edit(
                 content="도박 실패... 현재 돈: " + format(moneyA[idA.index(ID)], ",d") + "원"
             )
+            
+            
     if cmd == prefix + "랭킹" or cmd == prefix + "ㄹㅋ":
         rank, rankA = "", []  # 모든 id와 돈을 담아 정렬할 2차원 배열 rankA
         for i in range(0, len(idA)):
@@ -157,7 +161,7 @@ async def on_message(message):
                 break
         embed = discord.Embed(title="봇1시즌 돈 랭킹", description=rank, color=0xD8AA2D)
         await message.channel.send(embed=embed)
-
+        return
     if cmd == prefix + "레벨랭킹" or cmd == prefix + "ㄹㅂㄹㅋ":
         rank, rankA = "", []  # 모든 id와 돈을 담아 정렬할 2차원 배열 rankA
         for i in range(0, len(idA)):
@@ -170,7 +174,7 @@ async def on_message(message):
                 break
         embed = discord.Embed(title="봇1시즌 레벨 랭킹", description=rank, color=0xD8AA2D)
         await message.channel.send(embed=embed)
-
+        return
 
 
     if cmd == prefix + "송금" or cmd == prefix + "ㅅㄱ":
@@ -273,7 +277,7 @@ async def on_message(message):
         if ID in idA:
             levelA[idA.index(ID)] += level
             await message.channel.send(format(level) +" " + "level up 하셨습니다"+ "\n" +"현재 level : "+format(levelA[idA.index(ID)]))
-
+            
     if cmd == prefix + "레벨업팩2":
         if len(args) != 0:  # 인자 수가 잘못됬을 때
             embed = discord.Embed(title="오류", description="사용법: 레벨업팩(번호)", color=0xFF0000)
