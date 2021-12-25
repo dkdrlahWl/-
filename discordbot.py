d3d895dea23b44fbd987029042df7f05372b5cba
dkdrlahWl <63003501+dkdrlahWl@users.noreply.github.com>
2021-12-25 09:21:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -29,7 +29,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("------")
-    game = discord.Game("봇이랑 갠으로 가능")
+    game = discord.Game("veta.ver 초기화")
     await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
